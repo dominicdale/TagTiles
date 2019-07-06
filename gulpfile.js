1aed68f4ca8180ce7ae29d3235719d6090178f26
Dominic Dale <dominicdale@googlemail.com>
2019-07-06 09:57:07 +0100

    Init

---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var concat = require('gulp-concat');
+var browserSync = require('browser-sync').create();
+var gutil = require('gulp-util');
+var sourcemaps = require('gulp-sourcemaps');
+var version = require('gulp-version-number');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var cacheBuster = require('gulp-cache-bust');
+var htmlPartial = require('gulp-html-partial');
+var cat  = require('gulp-cat');
+const htmlmin = require('gulp-htmlmin');
+var autoprefixerOptions = {
+  browsers: ['Firefox < 20', 'ie 8-11', 'iOS 7', 'last 2 Chrome versions']
+};
+
+
+// less compiler
+gulp.task('less', function () {
+  return gulp.src('./src/css/*.less')
+    .pipe(sourcemaps.init())
+    .pipe(less({
+      compress: true
+    }).on('error', function (err) {
+      gutil.log(err);
+      this.emit('end');
+    }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(sourcemaps.write('./dist/sourcemaps'))
+    .pipe(gulp.dest('./dist/css/'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+
+
+// js compiler
+gulp.task('js', function () {
+  gulp.src([
+      './src/js/*.js'
+    ])
+    .pipe(sourcemaps.init())
+    .pipe(concat('compiled.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./sourcemaps'))
+    .pipe(gulp.dest('./dist/js/'))
+});
+
+
+// // cacheBuster Less
+// gulp.task('cacheLess', ['less'], function () {
+//   return gulp.src('./dist/index.html')
+//     .pipe(cacheBuster({
+//       type: 'timestamp'
+//     }))
+//     .pipe(gulp.dest('./'));
+// });
+
+
+// // cacheBuster JS
+// gulp.task('cacheJS', ['js'], function () {
+//   return gulp.src('./dist/index.html')
+//     .pipe(cacheBuster({
+//       type: 'timestamp'
+//     }))
+//     .pipe(gulp.dest('./'));
+// });
+
+
+// Partial
+gulp.task('partial', function () {
+  gulp.src(['./src/*.html'])
+    .pipe(htmlPartial({
+      basePath: './src/partials/'
+    }))
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('./dist'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+
+
+// browser reload
+gulp.task('browserSync', function () {
+  browserSync.init({
+    // proxy: 'sthree-components.local'
+    proxy: 'http://localhost:8888/TagTiles/dist'
+  })
+})
+
+
+
+// Watch
+gulp.task('watch', ['browserSync'], function () {
+  gulp.watch('./src/css/modules/*.less', ['less']);
+  gulp.watch('./src/css/*.less', ['less']);
+  gulp.watch('./src/js/*.js', ['js']);
+  gulp.watch('./src/**/*.html', ['partial']);
+});
+
+
+
+// Default task
+gulp.task('default', ['less', 'js', 'partial', 'watch']);
---
+++ src/js/99.scripts.js
@@ -0,0 +1,58 @@
+$(function () {
+
+    /* ==== Mobile nav ==== */
+    $('.nav__icon').click(function () {
+        $(this).toggleClass('nav__icon--active');
+        $(this).parent().find('.nav__dropdown').toggleClass('nav__dropdown--active');
+    });
+
+    $('.nav-toggle').click(function () {
+        $(this).toggleClass('nav-toggle--active');
+        $('nav').toggleClass('nav--open');
+        $('body').toggleClass('no-scroll');
+    });
+
+
+    /* ==== Add background to header on scroll ==== */
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 5) {
+            $("header").addClass('header--light');
+        } else {
+            $("header").removeClass('header--light');
+        }
+    });
+
+
+});
+
+
+
+$(window).on('load', function () {
+
+    // Remove loading window
+    setTimeout(function () {
+        $('main.loading-overlay').addClass('loading-overlay--hide');
+        // $('body').addClass('body--loaded');
+    }, 500);
+
+
+    // Add class if edge
+    if (/Edge/.test(navigator.userAgent)) {
+		$('body').addClass('ie ie--edge');
+	}
+
+    // Add class if IE
+	if (navigator.userAgent.indexOf('MSIE') !== -1 ||
+		navigator.appVersion.indexOf('Trident/') > -1) {
+        $('body').addClass('ie ie--11');
+        image_height();
+    }
+    
+    // Add mobile class
+    if ($(window).width() <= 480) {
+        $('body').addClass('breakpoint--mobile');
+    }
+
+});
